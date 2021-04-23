c39e4647591d23e7cb0a9a71c1c1cd68049e7937
2Succeed <74562298+2Succeed@users.noreply.github.com>
2021-04-23 10:56:21 -0600

    create planet website
    all new files

---
+++ planets/js/index.js
@@ -0,0 +1,27 @@
+// index.js
+//document.write("Hi I'm Working");
+
+const mercury = {
+    diameter:4879, 
+    distance:57.9,
+    image: "mercury.jpg"
+};
+
+const venus = {
+    diameter:12104, 
+    distance:108.2,
+    image: "venus.jpg"
+};
+
+for(let i=0; i<(mercury.distance/10); i++){
+    document.write('<br>');
+}
+
+document.write('<img src="images/mercury.jpg" width="'+mercury.diameter/1000+'">');
+
+for(let i=0; i<((venus.distance/10 - mercury.distance/10)); i++){
+    document.write('<br>');
+}
+
+document.write('<img src="images/venus.jpg" width="'+venus.diameter/1000+'">');
+
