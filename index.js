d463bee15fa965662217f99dfecd8ebde954a547
2Succeed <74562298+2Succeed@users.noreply.github.com>
2021-06-16 08:56:00 -0600

    splash page refresh
    Better fix to landing page.  needs additional tweaks.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 angular.module('myApp', []).controller('subjectCtrl', function($scope) {
     $scope.subjects = [
         {name:'Bingo', instructor:'Doug', title:'Online Bingo Cards', description:'Online Bingo Cards', url:"/bingo"},
-        {name:'Trivia', instructor:'Doug', title:'Trivia App for Social Hour', description:'Yeah Recipes', url:"/recipes"},
-        {name:'Recipes', instructor:'Ray', title:'2Succeed Home Cooked Recipes', description:'Ingredients', url:"/recipes"},
+        {name:'Trivia', instructor:'Doug', title:'Trivia App for Social Hour', description:'Yeah for Social Hour', url:"/trivia"},
+        {name:'Recipes', instructor:'Ray', title:'2Succeed Home Cooked Recipes', description:'Yeah for Recipes', url:"/recipes"}
     ];
-});
 
+    $scope.goto = function(url){
+        console.log("working "+ url);
+        location.href = url;
+    };
+});
 
