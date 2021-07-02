f9bf334fafe7ac32f56225e0fca0f9a688fb5b2a
2Succeed <74562298+2Succeed@users.noreply.github.com>
2021-07-02 11:26:17 -0600

    flags
    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 angular.module('myApp', []).controller('subjectCtrl', function($scope) {
     $scope.subjects = [
-        {name:'Bingo', instructor:'Doug', title:'Online Bingo Cards', description:'Online Bingo Cards', url:"/bingo"},
+        {name:'Bingo', instructor:'Doug', title:'Online Bingo Cards', description:'Online Bingo Cards', pic: "images/bingo.png", url:"/bingo"},
         {name:'Trivia', instructor:'Doug', title:'Trivia App for Social Hour', description:'Yeah for Social Hour', url:"/trivia"},
         {name:'Recipes', instructor:'Ray', title:'2Succeed Home Cooked Recipes', description:'Yeah for Recipes', url:"/recipes"}
     ];
